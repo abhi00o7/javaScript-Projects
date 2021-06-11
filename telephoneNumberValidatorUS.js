cf812d302908d1a4901c7ec0040ca2775c8778a7
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-06-11 09:13:14 +0530

    missing extension added

--- telephoneNumberValidatorUS.js
+++ telephoneNumberValidatorUS.js
@@ -56,85 +56,85 @@ function telephoneCheck(str) {
 
   //test cases   
 
-//   telephoneCheck("555-555-5555") should return a boolean.
+  telephoneCheck("555-555-5555") // should return a boolean.
 
 // Passed
-// telephoneCheck("1 555-555-5555") should return true.
+telephoneCheck("1 555-555-5555") // should return true.
 
 // Passed
-// telephoneCheck("1 (555) 555-5555") should return true.
+telephoneCheck("1 (555) 555-5555") // should return true.
 
 // Passed
-// telephoneCheck("5555555555") should return true.
+// telephoneCheck("5555555555") // should return true.
 
 // Passed
-// telephoneCheck("555-555-5555") should return true.
+// telephoneCheck("555-555-5555") // should return true.
 
 // Passed
-// telephoneCheck("(555)555-5555") should return true.
+// telephoneCheck("(555)555-5555") // should return true.
 
 // Passed
-// telephoneCheck("1(555)555-5555") should return true.
+// telephoneCheck("1(555)555-5555") // should return true.
 
 // Passed
-// telephoneCheck("555-5555") should return false.
+// telephoneCheck("555-5555") // should return false.
 
 // Passed
-// telephoneCheck("5555555") should return false.
+// telephoneCheck("5555555") // should return false.
 
 // Passed
-// telephoneCheck("1 555)555-5555") should return false.
+// telephoneCheck("1 555)555-5555") // should return false.
 
 // Passed
-// telephoneCheck("1 555 555 5555") should return true.
+// telephoneCheck("1 555 555 5555") // should return true.
 
 // Passed
-// telephoneCheck("1 456 789 4444") should return true.
+// telephoneCheck("1 456 789 4444") // should return true.
 
 // Passed
-// telephoneCheck("123**&!!asdf#") should return false.
+// telephoneCheck("123**&!!asdf#") // should return false.
 
 // Passed
-// telephoneCheck("55555555") should return false.
+// telephoneCheck("55555555") // should return false.
 
 // Passed
-// telephoneCheck("(6054756961)") should return false.
+// telephoneCheck("(6054756961)") // should return false.
 
 // Passed
-// telephoneCheck("2 (757) 622-7382") should return false.
+// telephoneCheck("2 (757) 622-7382") // should return false.
 
 // Passed
-// telephoneCheck("0 (757) 622-7382") should return false.
+// telephoneCheck("0 (757) 622-7382") // should return false.
 
 // Passed
-// telephoneCheck("-1 (757) 622-7382") should return false.
+// telephoneCheck("-1 (757) 622-7382") // should return false.
 
 // Passed
-// telephoneCheck("2 757 622-7382") should return false.
+// telephoneCheck("2 757 622-7382") // should return false.
 
 // Passed
-// telephoneCheck("10 (757) 622-7382") should return false.
+// telephoneCheck("10 (757) 622-7382") // should return false.
 
 // Passed
-// telephoneCheck("27576227382") should return false.
+// telephoneCheck("27576227382") // should return false.
 
 // Passed
-// telephoneCheck("(275)76227382") should return false.
+// telephoneCheck("(275)76227382") // should return false.
 
 // Passed
-// telephoneCheck("2(757)6227382") should return false.
+// telephoneCheck("2(757)6227382") // should return false.
 
 // Passed
-// telephoneCheck("2(757)622-7382") should return false.
+// telephoneCheck("2(757)622-7382") // should return false.
 
 // Passed
-// telephoneCheck("555)-555-5555") should return false.
+// telephoneCheck("555)-555-5555") // should return false.
 
 // Passed
-// telephoneCheck("(555-555-5555") should return false.
+// telephoneCheck("(555-555-5555") // should return false.
 
 // Passed
-// telephoneCheck("(555)5(55?)-5555") should return false.
+// telephoneCheck("(555)5(55?)-5555") // should return false.
 
 // Passed
-// telephoneCheck("55 55-55-555-5") should return false.
+// telephoneCheck("55 55-55-555-5") // should return false.
